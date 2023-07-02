508b7d9b37d7473c2df61c74aad06ecb2f6ee3f8
German <german98_xtv@hotmail.com>
2023-07-02 12:40:12 +0200

    api endpoints

---
+++ src/main/java/com/example/heroes/heroes/infraestructure/controller/HeroesGetController.java
@@ -0,0 +1,34 @@
+package com.example.heroes.heroes.infraestructure.controller;
+
+import com.example.heroes.heroes.application.HeroFinder;
+import com.example.heroes.heroes.application.HeroResponse;
+import com.example.heroes.heroes.application.HeroesSearcher;
+import com.example.heroes.shared.criteria.Criteria;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.List;
+
+@RestController
+@RequestMapping("/heroes")
+public final class HeroesGetController {
+
+    private final HeroFinder finder;
+    private final HeroesSearcher searcher;
+
+    public HeroesGetController(HeroFinder finder, HeroesSearcher searcher) {
+        this.finder = finder;
+        this.searcher = searcher;
+    }
+
+    @GetMapping
+    public List<HeroResponse> getHeroes(@RequestParam HashMap<String, Serializable> params) {
+        return searcher.search(Criteria.fromValues(params));
+    }
+
+    @GetMapping("/{id}")
+    public HeroResponse getHero(@PathVariable String id) {
+        return finder.find(id);
+    }
+}
