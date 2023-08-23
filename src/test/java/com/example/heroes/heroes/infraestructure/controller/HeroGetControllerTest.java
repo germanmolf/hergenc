5888aa9b1eb7c6405dd358d788b8d0918c84c214
German <german98_xtv@hotmail.com>
2023-08-23 12:17:09 +0200

    hero acceptance testing

--- src/test/java/com/example/heroes/heroes/infraestructure/controller/HeroGetControllerTest.java
+++ src/test/java/com/example/heroes/heroes/infraestructure/controller/HeroGetControllerTest.java
@@ -9,12 +9,20 @@ final class HeroGetControllerTest extends ControllerTestModule {
     void find_an_existing_hero() throws Exception {
         String id = "cc77f9b4-3209-11ee-be56-0242ac120002";
         String body = "{\"id\":\"" + id + "\",\"name\":\"superheroe\",\"power\":\"superpoder\"}";
-        givenThereIsAHero(id, body);
+        givenThereIsAHero(body);
 
         assertResponse(String.format("/heroes/%s", id), 200, body);
     }
 
-    private void givenThereIsAHero(String id, String body) throws Exception {
+    @Test
+    void not_find_a_non_existing_hero() throws Exception {
+        String id = "cc77f9b4-3209-11ee-be56-0242ac120002";
+        String body = "{\"code\":\"hero_not_found\",\"message\":\"Not found hero <cc77f9b4-3209-11ee-be56-0242ac120002>\"}";
+
+        assertResponse(String.format("/heroes/%s", id), 404, body);
+    }
+
+    private void givenThereIsAHero(String body) throws Exception {
         assertRequestWithBody("POST", "/heroes", body, 201);
     }
 }
