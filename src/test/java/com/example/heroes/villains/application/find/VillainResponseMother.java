24d509bba8471329b80878ad21ba0af34a1369f4
German <german98_xtv@hotmail.com>
2023-12-20 21:03:30 +0100

    villain endpoint get changes

--- src/test/java/com/example/heroes/villains/application/find/VillainResponseMother.java
+++ src/test/java/com/example/heroes/villains/application/find/VillainResponseMother.java
@@ -1,10 +1,17 @@
 package com.example.heroes.villains.application.find;
 
+import com.example.heroes.heroes.domain.HeroId;
 import com.example.heroes.villains.domain.Villain;
 
 public final class VillainResponseMother {
 
     public static VillainResponse fromAggregate(Villain villain) {
-        return new VillainResponse(villain.id().value(), villain.name().value(), villain.power().value());
+        return new VillainResponse(villain.id().value(),
+                villain.name().value(),
+                villain.power().value(),
+                villain.heroesDefeated().stream().map(HeroId::value).toList(),
+                villain.heroesDefeatedTotal().value(),
+                villain.status().value(),
+                villain.heroDefeater().map(HeroId::value).orElse(null));
     }
 }
--- src/test/java/com/example/heroes/villains/infraestructure/controller/VillainGetControllerTest.java
+++ src/test/java/com/example/heroes/villains/infraestructure/controller/VillainGetControllerTest.java
@@ -10,103 +10,177 @@ final class VillainGetControllerTest extends ControllerTestModule {
 
     @Test
     void find_an_existing_villain() throws Exception {
-        String id = "cc77f9b4-3209-11ee-be56-0242ac120002";
-        String body = "{\"id\":\"cc77f9b4-3209-11ee-be56-0242ac120002\",\"name\":\"supervillano\",\"power\":\"superpoder\"}";
-        givenThereIsAVillain(body);
+        givenThereIsAVillain("{\"id\":\"cc77f9b4-3209-11ee-be56-0242ac120002\",\"name\":\"supervillano\",\"power\":\"superpoder\"}");
+        String bodyResponse = "{\"id\":\"cc77f9b4-3209-11ee-be56-0242ac120002\"," +
+                "\"name\":\"supervillano\"," +
+                "\"power\":\"superpoder\"," +
+                "\"heroesDefeated\":[]," +
+                "\"heroesDefeatedTotal\":0," +
+                "\"status\":\"active\"}";
 
-        assertResponse(String.format("/villains/%s", id), 200, body);
+        assertResponse("/villains/cc77f9b4-3209-11ee-be56-0242ac120002", 200, bodyResponse);
     }
 
     @Test
     void not_find_a_non_existing_villain() throws Exception {
-        String id = "cc77f9b4-3209-11ee-be56-0242ac120002";
-        String body = "{\"code\":\"villain_not_found\",\"message\":\"Not found villain <cc77f9b4-3209-11ee-be56-0242ac120002>\"}";
+        String bodyResponse = "{\"code\":\"villain_not_found\",\"message\":\"Not found villain <cc77f9b4-3209-11ee-be56-0242ac120002>\"}";
 
-        assertResponse(String.format("/villains/%s", id), 404, body);
+        assertResponse("/villains/cc77f9b4-3209-11ee-be56-0242ac120002", 404, bodyResponse);
     }
 
     @Test
     void search_all_villains() throws Exception {
-        String joker = "{\"id\":\"cc77f9b4-3209-11ee-be56-0242ac120002\",\"name\":\"joker\",\"power\":\"está loco\"}";
-        String duendeVerde = "{\"id\":\"f555eb50-4827-11ee-be56-0242ac120002\",\"name\":\"duende verde\",\"power\":\"también loco\"}";
-        String body = "[" + joker + "," + duendeVerde + "]";
-        givenThereIsAVillain(joker);
-        givenThereIsAVillain(duendeVerde);
+        givenThereIsAVillain("{\"id\":\"cc77f9b4-3209-11ee-be56-0242ac120002\",\"name\":\"joker\",\"power\":\"está loco\"}");
+        givenThereIsAVillain("{\"id\":\"f555eb50-4827-11ee-be56-0242ac120002\",\"name\":\"duende verde\",\"power\":\"también loco\"}");
+        String body = "[" +
+                "{\"id\":\"cc77f9b4-3209-11ee-be56-0242ac120002\"," +
+                "\"name\":\"joker\"," +
+                "\"power\":\"está loco\"," +
+                "\"heroesDefeated\":[]," +
+                "\"heroesDefeatedTotal\":0," +
+                "\"status\":\"active\"}" +
+                "," +
+                "{\"id\":\"f555eb50-4827-11ee-be56-0242ac120002\"," +
+                "\"name\":\"duende verde\"," +
+                "\"power\":\"también loco\"," +
+                "\"heroesDefeated\":[]," +
+                "\"heroesDefeatedTotal\":0," +
+                "\"status\":\"active\"}" +
+                "]";
 
         assertResponse("/villains", 200, body);
     }
 
     @Test
     void search_by_name() throws Exception {
-        String joker = "{\"id\":\"cc77f9b4-3209-11ee-be56-0242ac120002\",\"name\":\"joker\",\"power\":\"está loco\"}";
-        String duendeVerde = "{\"id\":\"f555eb50-4827-11ee-be56-0242ac120002\",\"name\":\"duende verde\",\"power\":\"también loco\"}";
+        givenThereIsAVillain("{\"id\":\"cc77f9b4-3209-11ee-be56-0242ac120002\",\"name\":\"joker\",\"power\":\"está loco\"}");
+        givenThereIsAVillain("{\"id\":\"f555eb50-4827-11ee-be56-0242ac120002\",\"name\":\"duende verde\",\"power\":\"también loco\"}");
         String params = String.format(filterString, "name", "=", "joker");
 
-        String body = "[" + joker + "]";
-        givenThereIsAVillain(joker);
-        givenThereIsAVillain(duendeVerde);
+        String body = "[" +
+                "{\"id\":\"cc77f9b4-3209-11ee-be56-0242ac120002\"," +
+                "\"name\":\"joker\"," +
+                "\"power\":\"está loco\"," +
+                "\"heroesDefeated\":[]," +
+                "\"heroesDefeatedTotal\":0," +
+                "\"status\":\"active\"}" +
+                "]";
 
         assertResponse(String.format("/villains?%s", params), 200, body);
     }
 
     @Test
     void search_by_power() throws Exception {
-        String joker = "{\"id\":\"cc77f9b4-3209-11ee-be56-0242ac120002\",\"name\":\"joker\",\"power\":\"está loco\"}";
-        String duendeVerde = "{\"id\":\"f555eb50-4827-11ee-be56-0242ac120002\",\"name\":\"duende verde\",\"power\":\"también loco\"}";
+        givenThereIsAVillain("{\"id\":\"cc77f9b4-3209-11ee-be56-0242ac120002\",\"name\":\"joker\",\"power\":\"está loco\"}");
+        givenThereIsAVillain("{\"id\":\"f555eb50-4827-11ee-be56-0242ac120002\",\"name\":\"duende verde\",\"power\":\"también loco\"}");
         String params = String.format(filterString, "power", "=", "está loco");
 
-        String body = "[" + joker + "]";
-        givenThereIsAVillain(joker);
-        givenThereIsAVillain(duendeVerde);
+        String body = "[" +
+                "{\"id\":\"cc77f9b4-3209-11ee-be56-0242ac120002\"," +
+                "\"name\":\"joker\"," +
+                "\"power\":\"está loco\"," +
+                "\"heroesDefeated\":[]," +
+                "\"heroesDefeatedTotal\":0," +
+                "\"status\":\"active\"}" +
+                "]";
 
         assertResponse(String.format("/villains?%s", params), 200, body);
     }
 
     @Test
     void ignore_not_allowed_filter() throws Exception {
-        String joker = "{\"id\":\"cc77f9b4-3209-11ee-be56-0242ac120002\",\"name\":\"joker\",\"power\":\"está loco\"}";
-        String duendeVerde = "{\"id\":\"f555eb50-4827-11ee-be56-0242ac120002\",\"name\":\"duende verde\",\"power\":\"también loco\"}";
+        givenThereIsAVillain("{\"id\":\"cc77f9b4-3209-11ee-be56-0242ac120002\",\"name\":\"joker\",\"power\":\"está loco\"}");
+        givenThereIsAVillain("{\"id\":\"f555eb50-4827-11ee-be56-0242ac120002\",\"name\":\"duende verde\",\"power\":\"también loco\"}");
         String params = String.format(filterString, "id", "=", "cc77f9b4-3209-11ee-be56-0242ac120002");
 
-        String body = "[" + joker + "," + duendeVerde + "]";
-        givenThereIsAVillain(joker);
-        givenThereIsAVillain(duendeVerde);
+        String body = "[" +
+                "{\"id\":\"cc77f9b4-3209-11ee-be56-0242ac120002\"," +
+                "\"name\":\"joker\"," +
+                "\"power\":\"está loco\"," +
+                "\"heroesDefeated\":[]," +
+                "\"heroesDefeatedTotal\":0," +
+                "\"status\":\"active\"}" +
+                "," +
+                "{\"id\":\"f555eb50-4827-11ee-be56-0242ac120002\"," +
+                "\"name\":\"duende verde\"," +
+                "\"power\":\"también loco\"," +
+                "\"heroesDefeated\":[]," +
+                "\"heroesDefeatedTotal\":0," +
+                "\"status\":\"active\"}" +
+                "]";
 
         assertResponse(String.format("/villains?%s", params), 200, body);
     }
 
     @Test
     void order_by_name() throws Exception {
-        String villainA = "{\"id\":\"cc77f9b4-3209-11ee-be56-0242ac120002\",\"name\":\"a\",\"power\":\"está loco\"}";
-        String villainB = "{\"id\":\"f555eb50-4827-11ee-be56-0242ac120002\",\"name\":\"b\",\"power\":\"también loco\"}";
+        givenThereIsAVillain("{\"id\":\"cc77f9b4-3209-11ee-be56-0242ac120002\",\"name\":\"a\",\"power\":\"está loco\"}");
+        givenThereIsAVillain("{\"id\":\"f555eb50-4827-11ee-be56-0242ac120002\",\"name\":\"b\",\"power\":\"también loco\"}");
         String params = String.format(orderString, "name", "asc");
-        String body = "[" + villainA + "," + villainB + "]";
-        givenThereIsAVillain(villainA);
-        givenThereIsAVillain(villainB);
+        String body = "[" +
+                "{\"id\":\"cc77f9b4-3209-11ee-be56-0242ac120002\"," +
+                "\"name\":\"a\"," +
+                "\"power\":\"está loco\"," +
+                "\"heroesDefeated\":[]," +
+                "\"heroesDefeatedTotal\":0," +
+                "\"status\":\"active\"}" +
+                "," +
+                "{\"id\":\"f555eb50-4827-11ee-be56-0242ac120002\"," +
+                "\"name\":\"b\"," +
+                "\"power\":\"también loco\"," +
+                "\"heroesDefeated\":[]," +
+                "\"heroesDefeatedTotal\":0," +
+                "\"status\":\"active\"}" +
+                "]";
 
         assertResponse(String.format("/villains?%s", params), 200, body);
     }
 
     @Test
     void order_by_power() throws Exception {
-        String villainA = "{\"id\":\"cc77f9b4-3209-11ee-be56-0242ac120002\",\"name\":\"joker\",\"power\":\"a\"}";
-        String villainB = "{\"id\":\"f555eb50-4827-11ee-be56-0242ac120002\",\"name\":\"duende verde\",\"power\":\"b\"}";
+        givenThereIsAVillain("{\"id\":\"cc77f9b4-3209-11ee-be56-0242ac120002\",\"name\":\"joker\",\"power\":\"a\"}");
+        givenThereIsAVillain("{\"id\":\"f555eb50-4827-11ee-be56-0242ac120002\",\"name\":\"duende verde\",\"power\":\"b\"}");
         String params = String.format(orderString, "power", "asc");
-        String body = "[" + villainA + "," + villainB + "]";
-        givenThereIsAVillain(villainA);
-        givenThereIsAVillain(villainB);
+        String body = "[" +
+                "{\"id\":\"cc77f9b4-3209-11ee-be56-0242ac120002\"," +
+                "\"name\":\"joker\"," +
+                "\"power\":\"a\"," +
+                "\"heroesDefeated\":[]," +
+                "\"heroesDefeatedTotal\":0," +
+                "\"status\":\"active\"}" +
+                "," +
+                "{\"id\":\"f555eb50-4827-11ee-be56-0242ac120002\"," +
+                "\"name\":\"duende verde\"," +
+                "\"power\":\"b\"," +
+                "\"heroesDefeated\":[]," +
+                "\"heroesDefeatedTotal\":0," +
+                "\"status\":\"active\"}" +
+                "]";
 
         assertResponse(String.format("/villains?%s", params), 200, body);
     }
 
     @Test
     void ignore_not_allowed_orderBy() throws Exception {
-        String joker = "{\"id\":\"cc77f9b4-3209-11ee-be56-0242ac120002\",\"name\":\"joker\",\"power\":\"está loco\"}";
-        String duendeVerde = "{\"id\":\"f555eb50-4827-11ee-be56-0242ac120002\",\"name\":\"duende verde\",\"power\":\"también loco\"}";
+        givenThereIsAVillain("{\"id\":\"cc77f9b4-3209-11ee-be56-0242ac120002\",\"name\":\"joker\",\"power\":\"está loco\"}");
+        givenThereIsAVillain("{\"id\":\"f555eb50-4827-11ee-be56-0242ac120002\",\"name\":\"duende verde\",\"power\":\"también loco\"}");
         String params = String.format(orderString, "id", "desc");
-        String body = "[" + joker + "," + duendeVerde + "]";
-        givenThereIsAVillain(joker);
-        givenThereIsAVillain(duendeVerde);
+
+        String body = "[" +
+                "{\"id\":\"cc77f9b4-3209-11ee-be56-0242ac120002\"," +
+                "\"name\":\"joker\"," +
+                "\"power\":\"está loco\"," +
+                "\"heroesDefeated\":[]," +
+                "\"heroesDefeatedTotal\":0," +
+                "\"status\":\"active\"}" +
+                "," +
+                "{\"id\":\"f555eb50-4827-11ee-be56-0242ac120002\"," +
+                "\"name\":\"duende verde\"," +
+                "\"power\":\"también loco\"," +
+                "\"heroesDefeated\":[]," +
+                "\"heroesDefeatedTotal\":0," +
+                "\"status\":\"active\"}" +
+                "]";
 
         assertResponse(String.format("/villains?%s", params), 200, body);
     }
